6878fb048f7384c3a6b474d62175afec68ddcb57
ThiagoluisGaebler <111086192+ThiagoluisGaebler@users.noreply.github.com>
2022-08-12 13:02:27 -0300

    Add files via upload

---
+++ dados.php
@@ -0,0 +1,9 @@
+<?php
+    $tutoriais = array(
+        0 => array('id'=>'1', 'titulo'=>'Completa da casa', 'descricao'=>"Arroz, Feijão, Carne Assada e cozida, 2 tipos de Salada, Macarrão, Batata Frita",'imagem'=>'marmita01.png', 'tamanho' => "Pequena, Media e Grande", 'preco' => "R$ 10,00" ),
+        1 => array('id'=>'2', 'titulo'=>'Vegana', 'descricao'=>"Arroz, Feijão, Macarrão alho e oleo, 3 tipos de salada",'imagem'=>'marmita02.png', 'tamanho' => "Pequena, Media e Grande", 'preco' => "R$ 12,00"),
+        2 => array('id'=>'3', 'titulo'=>'Italiana', 'descricao'=>"Macarrão com almondegas", 'imagem'=>'marmita03.png', 'tamanho' => "Pequena, Media e Grande", 'preco' => "R$ 14,00"),
+        3 => array('id'=>'4', 'titulo'=>'Simples', 'descricao'=>"Arroz, Feijão, Carne cozida, Macarrão, Batata Frita",'imagem'=>'marmita04.png', 'tamanho' => "Pequena, Media e Grande", 'preco' => "R$ 12,00"),
+        4 => array('id'=>'5', 'titulo'=>'Para duas Pessoas', 'descricao'=>"Arroz, Feijão, Carne Assada e cozida,2 tipos de Salada, Macarrão, Batata Frita",'imagem'=>'marmita05.png', 'tamanho' => "Pequena, Media e Grande", 'preco' => "R$ 16,00"),
+    );
+
